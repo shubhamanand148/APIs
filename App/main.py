d72b1ab4b94c52030416389706e62025b2c889ed
Shubham Anand <shubhamanand148@gmail.com>
2023-02-01 01:12:56 +0530

    FastAPI CRUD Operation in postgres

--- App/main.py
+++ App/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 @app.get("/posts")
 async def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 #Creates Post
 #The default status code is 200, but it is recommended to use 201 while creating a data.
@@ -26,15 +26,15 @@ async def create_post(post: schemas.Post, db: Session = Depends(get_db)):
     db.commit()        #Saves the changes to the Database.
     db.refresh(new_post)
     
-    return {"Data": new_post}
+    return new_post
 
 @app.get("/posts/latest")
 async def get_latest_post(db: Session = Depends(get_db)):
 
     post = db.query(models.Post).order_by(models.Post.created_at.desc()).first()
-    return {"Latest Post: ": post}
+    return post
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.ResponsePost)
 async def post_by_id(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
@@ -43,7 +43,7 @@ async def post_by_id(id: int, db: Session = Depends(get_db)):
         #return {"message": f"Post with ID: {id} does not exist."}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail = f"Unable to find post with ID: {id} does not exist.")
-    return {"Post": post}
+    return post
 
 # HTTP Status 204 (No Content) indicates that the server has successfully fulfilled the request
 # and that there is no content to send in the response payload body.
@@ -72,7 +72,7 @@ async def update_post(id: int, post: schemas.Post, db: Session = Depends(get_db)
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail = f"Unable to Update post with ID: {id} does not exist.")
 
-    update_post.update(post.dict(), synchronize_session=False)
+    update_post.update({**post.dict()}, synchronize_session=False)
     db.commit()
 
-    return {"data": post}
+    return post
--- App/schemas.py
+++ App/schemas.py
@@ -1,11 +1,21 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
 class PostBase(BaseModel):
     title: str
-    content: str
     published: Optional[bool] = False
+
+#This is the model for the request which user sends to the server.
+class Post(PostBase):
+    content: str
     rating: Optional[int] = None
 
-class Post(PostBase):
-    pass
+
+#This is the model for the response which user gets back from the server.
+#In this project it is used only in "post/id"
+class ResponsePost(PostBase):
+    created_at: datetime
+
+    class Config:
+        orm_mode=True
